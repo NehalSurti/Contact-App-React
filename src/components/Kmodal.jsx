34047ff962e5d1605a8850f6d0b078cec638591d
NehalSurti <nehalsurti9998@gmail.com>
2024-01-23 23:40:32 +0000

    Adding files

---
+++ src/components/Kmodal.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+
+const Kmodal = ({ onOpen, isOpen, onClose, children, title }) => {
+  return (
+    <>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{title}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>{children}</ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default Kmodal;
